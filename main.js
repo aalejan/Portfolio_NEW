8fa1e4a4297e6705cdf2304d35a4cba0817f8ad1
Alex Alejandro <eaglesalejandro.aa@gmail.com>
2023-07-07 18:10:07 -0400

    added contact form. email is sent to me using emailJs

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+import emailJs from "emailjs-com";
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var serviceID = "service_4r45bgq";
+    var templateID = "template_pi9yffq";
+    var userID = "AG9C97xFIiLokwZb0";
+
+    emailJs.sendForm(serviceID, templateID, "#contactForm", userID).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Your message has been sent successfully!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        alert("Oops... " + JSON.stringify(error));
+      }
+    );
+  });
